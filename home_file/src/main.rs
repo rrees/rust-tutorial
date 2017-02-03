6cdfa6d13a78d1a61bffab498f06f4211d0e6a58
Robert Rees <shudderfix@gmail.com>
2017-02-03 21:10:58 +0000

    Creates a basic print function for the home directory

--- home_file/src/main.rs
+++ home_file/src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
     println!("Hello, world!");
+
+    match env::home_dir() {
+      Some(path) => print_dir(path),
+      None => println!("Home directory could not be read")
+    }
+}
+
+fn print_dir(directory_path: PathBuf) {
+    println!("{}", directory_path.display())
 }
